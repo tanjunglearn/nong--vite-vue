90fd2432e8775345cd279be0855de2b92ba41a1c
tanjunglearn <tjg.learn.programing@gmail.com>
2022-12-10 09:03:02 +0700

    feat: update package

--- src/features/login/Login.vue
+++ src/features/login/Login.vue
@@ -11,8 +11,8 @@
               name="email"
               type="email"
               autocomplete="email"
-              required
               placeholder="Email address"
+              required
               class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
           </div>
@@ -23,8 +23,8 @@
               name="password"
               type="password"
               autocomplete="current-password"
-              required
               placeholder="Password"
+              required
               class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
           </div>
